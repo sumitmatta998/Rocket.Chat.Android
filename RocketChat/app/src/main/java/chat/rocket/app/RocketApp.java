8b7f4f9d9f356c33a4ad8ce4f2a30e0b023d3b39
Julio Cotta <juliocbcotta@gmail.com>
2015-11-16 22:13:22 -0200

    added initialization of Meteor instance

--- RocketChat/app/src/main/java/chat/rocket/app/RocketApp.java
+++ RocketChat/app/src/main/java/chat/rocket/app/RocketApp.java
@@ -1,17 +1,34 @@
 package chat.rocket.app;
 
 import android.app.Application;
+import android.content.Context;
 import android.preference.PreferenceManager;
 
+import chat.rocket.network.Meteor;
+import chat.rocket.network.MeteorSingleton;
 import chat.rocket.network.PersistenceHandler;
 
 /**
  * Created by julio on 16/11/15.
  */
 public class RocketApp extends Application implements PersistenceHandler {
+
     @Override
     public void onCreate() {
         super.onCreate();
+        connect();
+    }
+
+    public static RocketApp get(Context context) {
+        return (RocketApp) context.getApplicationContext();
+    }
+
+    private void connect() {
+        // enable logging of internal events for the library
+        Meteor.setLoggingEnabled(true);
+
+        // create a new instance (protocol version in second parameter is optional)
+        MeteorSingleton meteor = MeteorSingleton.createInstance(this, "ws://android-ddp-meteor.meteor.com/websocket");
     }
 
     @Override
--- RocketChat/app/src/main/java/chat/rocket/app/ui/activities/HomeActivity.java
+++ RocketChat/app/src/main/java/chat/rocket/app/ui/activities/HomeActivity.java
@@ -2,8 +2,6 @@ package chat.rocket.app.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,11 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import chat.rocket.app.R;
+import chat.rocket.network.MeteorCallback;
+import chat.rocket.network.MeteorSingleton;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, MeteorCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +42,14 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        MeteorSingleton.getInstance().setCallback(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MeteorSingleton.getInstance().unsetCallback(this);
     }
 
     @Override
@@ -71,5 +80,35 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onConnect(boolean signedInAutomatically) {
+
+    }
+
+    @Override
+    public void onDisconnect(int code, String reason) {
+
+    }
+
+    @Override
+    public void onDataAdded(String collectionName, String documentID, String newValuesJson) {
+
+    }
+
+    @Override
+    public void onDataChanged(String collectionName, String documentID, String updatedValuesJson, String removedValuesJson) {
+
+    }
+
+    @Override
+    public void onDataRemoved(String collectionName, String documentID) {
+
+    }
+
+    @Override
+    public void onException(Exception e) {
+
     }
 }
