bce4cf5434bd7dc887ea3b7cfdb4e954ebdc67f4
developer- LOL <sumitmatta998@gmail.com>
2016-05-01 11:13:17 +0530

    Add Extra Button

--- VideoCalling/app/src/main/java/videocall/sumit/com/videocalling/MainActivity.java
+++ VideoCalling/app/src/main/java/videocall/sumit/com/videocalling/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import chat.rocket.app.ui.login.LoginActivity;
+import chat.rocket.app.ui.splash.SplashActivity;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
@@ -14,12 +17,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.rocketChat).setOnClickListener(this);
+        findViewById(R.id.rocketChatLoginActivity).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         Log.d("this", "onClick");
-        Intent i = new Intent(this,chat.rocket.app.ui.splash.SplashActivity.class);
-        startActivity(i);
+        switch (view.getId()) {
+            case R.id.rocketChat:
+                Intent i = new Intent(this, SplashActivity.class);
+                startActivity(i);
+                break;
+            case R.id.rocketChatLoginActivity:
+                Intent i1 = new Intent(this, LoginActivity.class);
+                startActivity(i1);
+                break;
+        }
     }
 }
